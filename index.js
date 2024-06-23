1c7bb4a55ba00681e7ad5f7555305029f16909e6
Turkan Suleimanova <turkan@Turkans-MacBook-Pro.local>
2024-06-23 21:23:26 +0300

    Add switch statment to make output messagese with base to randome number

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 inspMessages = () => {
   let messageNum = Math.floor(Math.random() * 2);
+
+  switch (messageNum) {
+    case 0:
+      console.log(
+        "Perfection is notattainable, but if we chase perfection we can catch excekkence."
+      );
+      break;
+    case 1:
+      console.log("Believe you can and you're halfway there.");
+      break;
+    case 2:
+      console.log("Happiness is not by chance, but by choice.");
+      break;
+    default:
+      console.log("Choose correct number!");
+  }
 };
 
 inspMessages();
